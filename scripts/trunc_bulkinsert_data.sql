a7218d3eb4229661ab0b99cc367ecc07d9be39bc
CharlesDLB <79206682+CharlesDLB@users.noreply.github.com>
2025-03-24 09:41:02 +0100

    Update trunc_bulkinsert_data.sql

--- scripts/trunc_bulkinsert_data.sql
+++ scripts/trunc_bulkinsert_data.sql
@@ -15,6 +15,7 @@ with (
 	,fieldterminator = ','
 	,tablock
 );
+
 truncate table bronze.crm_sales_details;
 bulk insert bronze.crm_sales_details
 from 'C:\Users\Charles\Documents\02_Projets perso\retail-etl-pipeline\sqlserver_mercedes\datasets\source_crm\sales_details.csv'
@@ -23,6 +24,7 @@ with (
 	,fieldterminator = ','
 	,tablock
 );
+
 truncate table bronze.erp_cust_az12;
 bulk insert bronze.erp_cust_az12
 from 'C:\Users\Charles\Documents\02_Projets perso\retail-etl-pipeline\sqlserver_mercedes\datasets\source_erp\CUST_AZ12.csv'
@@ -31,6 +33,7 @@ with (
 	,fieldterminator = ','
 	,tablock
 );
+
 truncate table bronze.erp_loc_a101;
 bulk insert bronze.erp_loc_a101
 from 'C:\Users\Charles\Documents\02_Projets perso\retail-etl-pipeline\sqlserver_mercedes\datasets\source_erp\LOC_A101.csv'
@@ -39,6 +42,7 @@ with (
 	,fieldterminator = ','
 	,tablock
 );
+
 truncate table bronze.erp_px_cat_g1v2;
 bulk insert bronze.erp_px_cat_g1v2
 from 'C:\Users\Charles\Documents\02_Projets perso\retail-etl-pipeline\sqlserver_mercedes\datasets\source_erp\PX_CAT_G1V2.csv'
