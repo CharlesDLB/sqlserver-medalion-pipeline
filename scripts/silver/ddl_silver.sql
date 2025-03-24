4acc024a0f7eff3d4c69ca6664d0146730c7ab28
CharlesDLB <79206682+CharlesDLB@users.noreply.github.com>
2025-03-24 17:59:52 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -14,8 +14,8 @@ create table silver.crm_cust_info(
 	cst_id int,
 	cst_key nvarchar(50),
 	cst_firstname nvarchar(50),
-	cst_last_name nvarchar(50),
-	cst_material_status nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_marital_status nvarchar(50),
 	cst_gndr nvarchar(50),
 	cst_create_date date
 );
